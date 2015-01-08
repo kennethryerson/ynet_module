0139d7571913faddf37973169d336db92f58326d
Kenneth Ryerson <kenneth.ryerson@gmail.com>
2015-01-08 08:59:02 -0600

    Added baud rate parameter to plcid

--- plcid.c
+++ plcid.c
@@ -8,10 +8,86 @@
 
 int plc_port = 0;
 
-int plc_open_port(char *port)
+struct speed_map {
+	unsigned short speed;
+	unsigned short value;
+};
+
+static const struct speed_map speeds[] = {
+	{B0, 0},
+	{B50, 50},
+	{B75, 75},
+	{B110, 110},
+	{B134, 134},
+	{B150, 150},
+	{B200, 200},
+	{B300, 300},
+	{B600, 600},
+	{B1200, 1200},
+	{B1800, 1800},
+	{B2400, 2400},
+	{B4800, 4800},
+	{B9600, 9600},
+#ifdef B19200
+	{B19200, 19200},
+#elif defined(EXTA)
+	{EXTA, 19200},
+#endif
+#ifdef B38400
+	{B38400, 38400/256 + 0x8000U},
+#elif defined(EXTB)
+	{EXTB, 38400/256 + 0x8000U},
+#endif
+#ifdef B57600
+	{B57600, 57600/256 + 0x8000U},
+#endif
+#ifdef B115200
+	{B115200, 115200/256 + 0x8000U},
+#endif
+#ifdef B230400
+	{B230400, 230400/256 + 0x8000U},
+#endif
+#ifdef B460800
+	{B460800, 460800/256 + 0x8000U},
+#endif
+#ifdef B921600
+	{B921600, 921600/256 + 0x8000U},
+#endif
+};
+
+enum { NUM_SPEEDS = sizeof(speeds)/sizeof(speeds[0]) };
+
+static unsigned tty_baud_to_value(speed_t speed)
 {
-    unsigned int baud = B115200;
-    
+	int i = 0;
+
+	do {
+		if (speed == speeds[i].speed) {
+			if (speeds[i].value & 0x8000U) {
+				return ((unsigned long) (speeds[i].value) & 0x7fffU) * 256;
+			}
+			return speeds[i].value;
+		}
+	} while (++i < NUM_SPEEDS);
+
+	return 0;
+}
+
+static speed_t tty_value_to_baud(unsigned int value)
+{
+	int i = 0;
+
+	do {
+		if (value == tty_baud_to_value(speeds[i].speed)) {
+			return speeds[i].speed;
+		}
+	} while (++i < NUM_SPEEDS);
+
+	return (speed_t) - 1;
+}
+
+int plc_open_port(char *port, speed_t baud)
+{   
     /* open IT900 serial port */
     plc_port = open(port, O_RDWR | O_NOCTTY);
     if(plc_port < 0)
@@ -128,16 +204,31 @@ int plc_get_id(unsigned short *netid, unsigned short *id)
 int main(int argc, char **argv)
 {
     unsigned short netid, id;
+    speed_t baud;
+    int tty_value = -1;
     
-    if(argc != 2)
+    if(argc != 3)
     {
-        fprintf(stderr,"Usage: plcid DEVICE\n");
+        fprintf(stderr,"Usage: plcid DEVICE BAUD\n");
         return 1;
     }
     
     char *port = argv[1];
     
-    if(plc_open_port(port))
+    tty_value = atoi(argv[2]);
+	if(tty_value < 0)
+	{
+		fprintf(stderr,"Invalid baud rate: %s\n",argv[2]);
+		return 1;
+	}
+	baud = tty_value_to_baud(tty_value);
+	if(baud < 0)
+	{
+		fprintf(stderr,"Invalid baud rate: %s\n",argv[2]);
+		return 1;
+	}
+    
+    if(plc_open_port(port,baud))
     {
         fprintf(stderr,"Failed to open port\n");
         plc_close_port();
